2040b048dcc3975630aeb67c777442c1c7b367ed
masendhy <massendhy@gmail.com>
2022-04-21 08:45:00 +0700

    Navbar and Welcome Screen done

--- client/vite-project/src/components/Navbar.jsx
+++ client/vite-project/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import logo from '../../images/logo.png'
+import logo from '../../images/LEK.svg'
 import React from 'react';
 
 const NavbarItem= ({title, classProps})=>{
@@ -30,18 +30,18 @@ const Navbar =()=> {
 
             <div className='flex relative '>
             {toggleMenu
-? <AiOutlineClose fontSize={27} className='text-white md:hidden cursor-pointer' onClick={() => setTogglemenu(false)} />
+? <AiOutlineClose fontSize={27} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
 :<HiMenuAlt4 fontSize={27} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)}   />
 }
 {toggleMenu &&(
     <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none'>
     <li className='text-xl w-full my-2
-    flex flex-col justify-start item-end rounded-md blue-glassmorphism text-white animate-slide-in
+    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
     '>
-<AiOutlineClose onClick={()=> setToggleMenu(false)}/>
-{['Market', 'Exchange','Tutorials','Wallets'].map((item, index)=>(
+<AiOutlineClose onClick={()=> setToggleMenu(false)} className='my-2 mx-2' />
+{['Market', 'Exchange','Tutorials','Wallets'].map((item, index)=>
                <NavbarItem key={item + index} title={item} classProps='my-2 text-lg'/>
-           ))}
+           )}
     </li>
      </ul>
 ) }
